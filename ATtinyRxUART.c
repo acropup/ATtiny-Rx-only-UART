4116a6f95f1351191726c7911b2e360d4c245f3b
Shane Burgess <acropup@users.noreply.github.com>
2015-08-06 10:34:11 -0700

    Adapted code for both ATtiny84 and ATtiny85

--- ATtinyRxUART.c
+++ ATtinyRxUART.c
@@ -1,16 +1,38 @@
-/* ATtiny85 USI UART v3
+/* ATtiny84/85 USI UART v3
+   This is a Receive-only UART for ATtiny84/85 that uses the hardware USI
+   in three-wire (SPI) mode to receive bytes. The UART MOSI line should be 
+   connected to the chip's MOSI/DI pin. MISO/DO and SCK/SCL pins are not used.
 
+   Original code and description: http://www.technoblogy.com/show?VSX
    David Johnson-Davies - www.technoblogy.com - 6th May 2015
    and Edgar Bonet - www.edgar-bonet.org
-   ATtiny85 @ 8MHz (external crystal; BOD disabled)
+   ATtiny84/85 @ 8MHz (external crystal; BOD disabled)
    
    CC BY 4.0
    Licensed under a Creative Commons Attribution 4.0 International license: 
    http://creativecommons.org/licenses/by/4.0/
 */
 
+#if defined(__AVR_ATtiny84__)
+  #define ATTINY_PIN        PINA
+  #define ATTINY_PIN_MOSI   PINA6
+  #define ATTINY_PCINT_MOSI PCINT6
+  #define ATTINY_PCIE       PCIE0
+  #define ATTINY_PCIF       PCIF0
+  #define ATTINY_PCMSK      PCMSK0
+#elseif defined(__AVR_ATtiny85__)
+  #define ATTINY_PIN        PINB
+  #define ATTINY_PIN_MOSI   PINB0
+  #define ATTINY_PCINT_MOSI PCINT0
+  #define ATTINY_PCIE       PCIE
+  #define ATTINY_PCIF       PCIF
+  #define ATTINY_PCMSK      PCMSK
+#else
+  #error Must build for ATtiny84 or ATtiny85.
+#endif
+
 // Constant
-const int DataIn = PINB0;         // USI DI
+const int DataIn = ATTINY_PIN_MOSI;   // USI DI
 
 // USI UART **********************************************
 
@@ -25,15 +47,15 @@ unsigned char ReverseByte (unsigned char x) {
 void InitialiseUSI (void) {  
   DDRB &= ~(1<<DataIn);           // Define DI as input
   USICR = 0;                      // Disable USI.
-  GIFR = 1<<PCIF;                 // Clear pin change interrupt flag.
-  GIMSK |= 1<<PCIE;               // Enable pin change interrupts
-  PCMSK |= 1<<PCINT0;             // Enable pin change on pin 0
+  GIFR = 1<<ATTINY_PCIF;                 // Clear pin change interrupt flag.
+  GIMSK |= 1<<ATTINY_PCIE;               // Enable pin change interrupts
+  ATTINY_PCMSK |= 1<<ATTINY_PCINT_MOSI;  // Enable pin change on DI/MOSI pin
 }
 
 // Pin change interrupt detects start of UART reception.
 ISR (PCINT0_vect) {
-  if (PINB & 1<<DataIn) return;   // Ignore if DI is high
-  GIMSK &= ~(1<<PCIE);            // Disable pin change interrupts
+  if (ATTINY_PIN & 1<<DataIn) return;    // Ignore if DI is high
+  GIMSK &= ~(1<<ATTINY_PCIE);     // Disable pin change interrupts
   TCCR0A = 2<<WGM00;              // Timer in CTC mode
   TCCR0B = 2<<CS00;               // Set prescaler to /8
   OCR0A = 103;                    // Shift every (103+1)*8 cycles
@@ -48,8 +70,8 @@ ISR (USI_OVF_vect) {
   USICR = 0;                      // Disable USI         
   int temp = USIDR;
   Display(ReverseByte(temp));
-  GIFR = 1<<PCIF;                 // Clear pin change interrupt flag.
-  GIMSK |= 1<<PCIE;               // Enable pin change interrupts again
+  GIFR = 1<<ATTINY_PCIF;          // Clear pin change interrupt flag.
+  GIMSK |= 1<<ATTINY_PCIE;        // Enable pin change interrupts again
 }
 
 void Display (char c) {
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is a compact and efficient receive-only Serial UART for the ATtiny84 and AT
 based on the work by David Johnson-Davies and Edgar Bonet. See the original work at 
 http://www.technoblogy.com/show?VSX. It has been adapted such that the ATtiny84 and 85
 code base is combined.
+
+This is a Receive-only UART for ATtiny84/85 that uses the hardware USI in three-wire (SPI) 
+mode to receive bytes. The UART MOSI line should be connected to the chip's MOSI/DI pin. 
+MISO/DO and SCK/SCL pins are not used.
